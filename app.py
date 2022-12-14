5142ff60060ce0a9f04d7800a0e3c965434ee96e
MiMiRinKo <frank23671199@gmail.com>
2022-12-15 00:15:25 +0800

    waiting_for_model

--- app.py
+++ app.py
@@ -30,6 +30,7 @@ def pred(name,fy,city,nation,industry,founder,investory,tf,ct,os,ls):
     # last valuation
 
     # length of company name
+    name_length = 0
     name_length = ("".join(name)).__len__()
     # special char counts of company name
     name_sp = count_special_char("".join(name))
@@ -53,13 +54,57 @@ def pred(name,fy,city,nation,industry,founder,investory,tf,ct,os,ls):
 
     #this sould be button or scroll to choose
     industry_type = ("".join(industry)).lower()
-    industry: dict = {'artificial intelligence': 0, 'fintech': 1, 'internet software & services': 2, 'analytics': 3, 'biotechnology': 4, 'health care': 5, 'e-commerce & direct-to-consumer': 6}
+    #print(industry_type)
+    #default
+    industry_value = 0
+    #industry: dict = {'artificial intelligence': 0, 'fintech': 1, 'internet software & services': 2, 'analytics': 3, 'biotechnology': 4, 'health care': 5, 'e-commerce & direct-to-consumer': 6}
+    match industry_type:
+        case "artificial intelligence":
+            industry_value = 0
+        case "fintech":
+            industry_value = 1
+        case "internet software & services":
+            industry_value = 2
+        case "analytics":
+            industry_value = 3
+        case "biotechnology":
+            industry_value = 4
+        case "health care":
+            industry_value = 5
+        case "e-commerce & direct-to-consumer":
+            industry_value = 6
+    #test get value
+    #print(industry_value)
 
     #this sould be button or scroll to choose
     investor_type = ("".join(investory)).lower()
+    #default
+    investor_value = 0
+
+    #print(investor_type)
+
     investory: dict = {'andreessen horowitz': 0, 'techstars': 1, 'alumni ventures': 2, 'y combinator': 3, 'sequoia capital': 4, '500 global': 5, 'insight partners': 6}
+    match investor_type:
+        case "andreessen horowitz":
+            investor_value = 0
+        case "techstars":
+            investor_value = 1
+        case "alumni ventures":
+            investor_value = 2
+        case "y combinator":
+            investor_value = 3
+        case "sequoia capital":
+            investor_value = 4
+        case "500 global":
+            investor_value = 5
+        case "insight partners":
+            investor_value = 6
+
+    #test get value
+    #print(investor_value)
 
     #last valuation
+    last_valuation_value = 0
     last_valuation_value = ("".join(ls)).__len__()-1
 
     output = "here should be prediction"
