9120cf8083462fdd26c8f677d819d75624845143
mikaiyen <mikai890820@gmail.com>
2022-12-14 22:59:16 +0800

    ugly-ass html orz

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//這邊必須要async funciton 因為python返回需要時間，而JS 又不會block，
+//所以需要用async function 加上await去呼叫PY function
 async function fit(){
     var name = document.getElementById("name").value
     var fy = document.getElementById("Founded_Year").value
@@ -10,8 +12,11 @@ async function fit(){
     var ct = document.getElementById("Company_Type").value
     var os = document.getElementById("Operating_Status").value
     var ls = document.getElementById("Last_Valuation").value
-    eel.pred(name,fy,city,nation,industry,founder,investory,tf,ct,os,ls)(call_Back)
-}
+    var result = eel.pred(name,fy,city,nation,industry,founder,investory,tf,ct,os,ls)
+    result(call_Back) // a little bit weird https://youtu.be/IbGJaTkyuXA?t=239
+}   
+
+eel.expose(call_Back)
 
 function call_Back(output){
     document.getElementById("res").textContent = output
